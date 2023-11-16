839371a880d39ace97b08f19437da5abc38d4a6d
Wiebe <twiebe@rwi-essen.de>
2023-11-16 20:58:14 +0100

    working hybrid

--- R/make_/make_hybrid.R
+++ R/make_/make_hybrid.R
@@ -43,33 +43,61 @@ make_X_3 = function(x_conts = NA, x_star_conts = NA , x_binaries = NA, x_star_bi
     x_binaries = unlist(x_binaries)
     x_star_binaries = unlist(x_star_binaries)
 
-    x_conts_pre = log(x_star_consts/x_consts))
-    x_conts_sub = (t_month* log(x_star_consts)) - (T_month* log(x_consts))
+    x_conts_pre = log(x_star_conts/x_conts)
+    x_conts_sub = (t_month* log(x_star_conts)) - (T_month* log(x_conts))
 
-    x_binaries_pre = x_star_consts - x_consts
-    x_binaries_sub = (t_month* x_star_consts) - (T_month* x_consts)
+    x_binaries_pre = x_star_binaries - x_binaries
+    x_binaries_sub = (t_month* x_star_binaries) - (T_month* x_binaries)
 
     X_3 = c(0, x_conts_pre, x_binaries_pre, x_conts_sub, x_binaries_sub)   
-    
-    total_length = sum(sapply(append(x_conts,x_binaries),length) * 2) + 1
 
+    total_length = sum(length(c(x_conts,x_binaries)) * 2) + 1
     tar_assert_true(length(X_3) == total_length)
     return(X_3)
 }
 
 # make some example data
-x_1 = runif(2) # cont
-x_star_1 = x_1 + runif(2)
-x_2 = runif(2) # cont
-x_star_2 = x_2 + runif(2)
-x_3 = c(0,1) # binary
-x_star_3 = as.numeric(!x_3)
-V_t = c(100,200) # prev_price_var
-t = c(200,300) # prev_date
-T = c(210,320) # date
-V_T = c(200,250) # price_var
+# order is hedonic, unchanged rs, changed rs
+x_1 = c(4,5,6) # cont
+x_star_1 = x_1 + c(1,0,1)
+x_2 = c(2,3,4) # cont
+x_star_2 = x_2 + c(1,0,1)
+x_3 = c(1,0,1) # binary
+x_star_3 = x_3 + c(-1,0,-1)
+
+# hedonic
+V_h_t = 50 
+h_t = 150
+# unchanged_rs
+V_urs_t = 100
+V_urs_T = 180
+urs_t = 200
+urs_T = 210
+
+# changed_rs
+V_crs_t = 200
+V_crs_T = 150
+crs_t = 180
+crs_T = 185
 
 
 
+Y = log(
+    c(
+        V_h_t, 
+        (V_urs_t/V_urs_T), 
+        (V_crs_t/V_crs_T)
+    )
+)
+Z = do.call(rbind, list(
+    make_X_1(x_conts = list(x_1,x_2), x_binaries = list(x_3), t_month = h_t),
+    make_X_2(x_conts = list(x_1,x_2), x_binaries = list(x_3), t_month = urs_t, T_month = urs_T),
+    make_X_3(x_conts = list(x_1,x_2), x_star_conts = list(x_star_1,x_star_2), x_binaries = list(x_3), x_star_binaries = list(x_star_3), t_month = crs_t, T_month = crs_T)
+)
+)
+#beta = crossprod(Z)
+beta = qr.coef(qr(Z), Y)
+# or
+lm(Y ~ Z -1)
 
 }
