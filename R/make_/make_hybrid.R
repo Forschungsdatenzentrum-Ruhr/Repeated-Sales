18e3349fd384466dfe517f7e109b8d192bdd5175
Wiebe <twiebe@rwi-essen.de>
2024-01-04 18:47:44 +0100

    hybrid iteration X_1,X_2,X_3)

--- R/make_/make_hybrid.R
+++ R/make_/make_hybrid.R
@@ -4,7 +4,7 @@ make_hybrid = function(RED_classified,self_merged_rs_pairs, data_type){
   depVar = list_var$depVar
   indepVar = list_var$indepVar
   # think of a solution for this, they are mutated in prepare_hedonic
-  var_to_keep = c(setdiff(indepVar,c("declared_wohngeld", "baujahr_cat", "first_occupancy", "num_floors", "floors_cat")),"rs_id")
+  var_to_keep = c(setdiff(indepVar,c("declared_wohngeld", "baujahr_cat", "first_occupancy", "num_floors", "floors_cat")),"rs_id","emonths")
 
 # one consideration is that we have to decide between using or dropping updates from hedonic as well
 
@@ -37,8 +37,8 @@ changed_boolean = pure_rs[,
 
 tar_assert_true(length(changed_boolean) == nrow(pure_rs))
 
-pure_rs[,changed_to := changed_boolean][, changed_from := lead(changed_to,1), by = rs_id]
-
+pure_rs[, changed_to := changed_boolean][, changed_from := lead(changed_to,1), by = rs_id]
+# this pretty much allows for duplicate indiviudal listings between pure/changed
 # sample 1 pure rs
 pure_pairs = pure_rs[changed_to == FALSE | changed_from == FALSE]
 
@@ -51,46 +51,72 @@ hedonic_listings = RED_classified[hybrid_type == "hedonic", ..var_to_keep]
 binary_names = c("balkon","garten","einbaukueche","gaestewc","aufzug","keller","betreut")
 cont_names = c("ausstattung","zimmeranzahl")
 
-make_X_1 = function(x_conts = NA , x_binaries = NA, t_month = NA){
-    x_conts = unlist(x_conts)
-    x_binaries = unlist(x_binaries)
-    
-    x_conts_pre = log(x_conts)
-    x_conts_sub = t_month * x_conts_pre
+# hedonic
+X_1 = make_X_1(full_data = hedonic_listings, x_conts = cont_names, x_binaries = binary_names, t_month = "emonths")
+# pure
+X_2 = make_X_2(pure = pure_pairs,x_conts = cont_names, x_binaries = binary_names, t_month = "emonths", T_month = "emonths")
+# changed
+X_3 = make_X_3(
+    changed = changed_pairs,
+    x_conts = cont_names, 
+    x_star_conts = cont_names, 
+    x_binaries = binary_names, 
+    x_star_binaries = binary_names, 
+    t_month = "emonths", 
+    T_month = "emonths"
+)
 
-    x_binaries_pre = x_binaries
-    x_binaries_sub = t_month * x_binaries_pre
+make_X_1 = function(full_data = NA, x_conts = NA , x_binaries = NA, t_month = NA){
 
-    X_1 = c(1, x_conts_pre, x_binaries_pre, x_conts_sub, x_binaries_sub)   
+    x_conts = full_data[,..x_conts]
+    x_binaries = full_data[,..x_binaries]
+    t_month = full_data[[t_month]]
+
     
-    total_length = sum(length(c(x_conts,x_binaries)) * 2) + 1
+    x_conts_pre = x_conts[,lapply(.SD, function(x){log(x)})]
+    x_conts_sub = x_conts[,lapply(.SD, function(x){t_month * x})]
+
+    x_binaries_pre = x_binaries[,lapply(.SD, function(x){x})]
+    x_binaries_sub = x_binaries[,lapply(.SD, function(x){t_month * x})]
+    
+    X_1 = data.table(constant = rep(1,nrow(full_data)))
+    X_1 = cbind(X_1,x_conts_pre, x_binaries_pre, x_conts_sub, x_binaries_sub)     
+    total_length = sum(length(c(x_conts, x_binaries)) * 2) + 1
 
     tar_assert_true(length(X_1) == total_length)
     return(X_1)
 }
-make_X_2 = function(x_conts = NA , x_binaries = NA, t_month = NA, T_month = NA){
-    x_conts = unlist(x_conts)
-    x_binaries = unlist(x_binaries)
+make_X_2 = function(pure = NA, x_conts = NA , x_binaries = NA, t_month = NA, T_month = NA){
 
-    x_conts_pre = rep(0,length(x_conts))
-    x_conts_sub = (t_month-T_month) * log(x_conts)
+    x_conts = pure[,..x_conts]
+    x_binaries = pure[,..x_binaries]
+    t_month = pure[[t_month]]
+    # this makes no sense but works
+    T_month = pure[,lag("emonths",1), by = "rs_id"][[T_month]]
 
-    x_binaries_pre = rep(0,length(x_binaries))
-    x_binaries_sub = (t_month-T_month) * x_binaries
+    x_conts_pre = matrix(0,length(x_conts),nrow(x_conts)) |> as.data.table()
+    x_conts_sub = xconts[,lapply(.SD, function(x){log((t_month-T_month) * log(x))})]
+    
+    x_binaries_pre = matrix(0,length(x_binaries),nrow(x_binaries)) |> as.data.table()
+    x_binaries_sub = x_binaries[,lapply(.SD, function(x){(t_month-T_month) * x})]
 
-    X_2 = c(0, x_conts_pre, x_binaries_pre, x_conts_sub, x_binaries_sub)   
+    X_2 = data.table(constant = rep(0,nrow(full_data)))
+    X_2 = c(X_2, x_conts_pre, x_binaries_pre, x_conts_sub, x_binaries_sub)   
     
     total_length = sum(length(c(x_conts,x_binaries)) * 2) + 1
 
     tar_assert_true(length(X_2) == total_length)
     return(X_2)
 }
-make_X_3 = function(x_conts = NA, x_star_conts = NA , x_binaries = NA, x_star_binaries = NA, t_month = NA, T_month = NA){
-    x_conts = unlist(x_conts)
-    x_star_conts = unlist(x_star_conts)
-    x_binaries = unlist(x_binaries)
-    x_star_binaries = unlist(x_star_binaries)
+make_X_3 = function(changed = NA, x_conts = NA, x_star_conts = NA , x_binaries = NA, x_star_binaries = NA, t_month = NA, T_month = NA){
 
+    x_conts = changed[,..x_conts]
+    x_star_conts = changed[,lapply(.SD,function(x){lag(x,1)}), by = "rs_id", .SDcols = x_star_conts][,rs_id := NULL]
+    x_binaries = changed[,..x_binaries]
+    x_star_binaries = changed[,lapply(.SD,function(x){lag(x,1)}), by = "rs_id", .SDcols = x_star_binaries][,rs_id := NULL]
+    t_month = changed[[t_month]]
+    T_month = changed[,lag("emonths",1), by = "rs_id"][[T_month]]
+    # stopped here
     x_conts_pre = log(x_star_conts/x_conts)
     x_conts_sub = (t_month* log(x_star_conts)) - (T_month* log(x_conts))
 
