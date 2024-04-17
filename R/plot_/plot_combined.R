04f75724389c4ec36c8b057ea36f867397920695
thorbenwiebe <thorben.wiebe@rwi-essen.de>
2024-04-17 16:54:57 +0200

    formatting plot_

--- R/plot_/plot_combined.R
+++ R/plot_/plot_combined.R
@@ -1,19 +1,35 @@
-plot_combined = function(combined_indices, data_type){
-    
-    # default
-    plot1 = ggplot(combined_indices) +
-    geom_line( aes(x = date_quarter, y = based_index, color = index_type, group = index_type)) +
+plot_combined <- function(combined_indices, data_type) {
+  #' @title Plot combined indices
+  #'
+  #' @description Plot combined indices
+  #' @param combined_indices data.table. Combined indices data set.
+  #' @param data_type character. Type of data to be used. One of:
+  #' *WK = Wohnungskauf/ Apartments for sale
+  #' *WM = Wohnungsmiete/ Apartments for rent
+  #' *HK = Hauskauf/ Houses for sale
+  #'
+  #' @return NULL
+  #' @author Thorben Wiebe
+  # ----------------------------------------------
+  # Input validation
+  input_check(combined_indices, "data.table")
+  input_check(data_type, "character")
+  #----------------------------------------------
+
+  # default
+  plot1 <- ggplot(combined_indices) +
+    geom_line(aes(x = date_quarter, y = based_index, color = index_type, group = index_type)) +
     coord_cartesian(expand = FALSE) +
     theme_bw()
-    ggsave(glue::glue("output/figures/{data_type}_combined_indicies.png"), plot1, width = 10, height = 10)
-    
-    # smooth
-    plot2 = ggplot(combined_indices) + 
-      stat_smooth( aes(x = date_quarter, y = based_index, color = index_type, group = index_type), formula = y ~ s(x, bs = "cs"), method = "gam", se = F) +
-      coord_cartesian(expand = FALSE) +
-      theme_bw()
-    ggsave(glue::glue("output/figures/{data_type}_smooth_combined_indicies.png"), plot2, width = 10, height = 10)
+  ggsave(glue::glue("output/figures/{data_type}_combined_indicies.png"), plot1, width = 10, height = 10)
 
-    return(NULL)
+  # smooth
+  plot2 <- ggplot(combined_indices) +
+    stat_smooth(aes(x = date_quarter, y = based_index, color = index_type, group = index_type), formula = y ~ s(x, bs = "cs"), method = "gam", se = F) +
+    coord_cartesian(expand = FALSE) +
+    theme_bw()
+  ggsave(glue::glue("output/figures/{data_type}_smooth_combined_indicies.png"), plot2, width = 10, height = 10)
+
+  #----------------------------------------------
+  return(NULL)
 }
- 
--- R/plot_/plot_split.R
+++ R/plot_/plot_split.R
@@ -1,26 +1,39 @@
-plot_split = function(indicies, data_type){
+plot_split <- function(indicies, data_type) {
+  #' @title Plot split indices
+  #'
+  #' @description Plot split indices
+  #' @param indicies data.table. Split indices data set.
+  #' @param data_type character. Type of data to be used. One of:
+  #' *WK = Wohnungskauf/ Apartments for sale
+  #' *WM = Wohnungsmiete/ Apartments for rent
+  #' *HK = Hauskauf/ Houses for sale
+  #'
+  #' @return NULL
+  #' @author Thorben Wiebe
+  #----------------------------------------------
+  # Input validation
+  input_check(indicies, "data.table")
+  input_check(data_type, "character")
+  #----------------------------------------------
 
-for(single_index_type in unique(indicies$index_type)){
+  for (single_index_type in unique(indicies$index_type)) {
+    # subset data
+    single_index <- indicies[index_type == single_index_type]
 
-        # subset data
-        single_index = indicies[index_type == single_index_type]
+    # default
+    plot1 <- ggplot(single_index, aes(x = date_quarter, y = based_index, color = gid2019, group = gid2019)) +
+      geom_line(aes(x = date_quarter, y = based_index, color = gid2019, group = gid2019)) +
+      coord_cartesian(expand = FALSE) +
+      theme_bw()
+    ggsave(glue::glue("output/figures/{data_type}_{single_index_type}_gid_indicies.png"), plot1, width = 10, height = 10)
 
-        # default
-        plot1 = ggplot(single_index, aes(x = date_quarter, y = based_index, color = gid2019, group = gid2019)) + 
-        geom_line(aes(x = date_quarter, y = based_index, color = gid2019, group = gid2019)) +
-        coord_cartesian(expand = FALSE) + 
-        theme_bw()
-        ggsave(glue::glue("output/figures/{data_type}_{single_index_type}_gid_indicies.png"), plot1, width = 10, height = 10)
-        
-        # smmoth
-        plot2 = ggplot(single_index, aes(x = date_quarter, y = based_index, color = gid2019, group = gid2019)) + 
-          stat_smooth(aes(x = date_quarter, y = based_index, color = gid2019, group = gid2019), formula = y ~ s(x, bs = "cs"), method = "gam", se = F) +
-          coord_cartesian(expand = FALSE) + 
-          theme_bw()
-        ggsave(glue::glue("output/figures/{data_type}_{single_index_type}_smooth_gid_indicies.png"), plot2, width = 10, height = 10)
-
-    }
-
-    return(NULL)
-
+    # smmoth
+    plot2 <- ggplot(single_index, aes(x = date_quarter, y = based_index, color = gid2019, group = gid2019)) +
+      stat_smooth(aes(x = date_quarter, y = based_index, color = gid2019, group = gid2019), formula = y ~ s(x, bs = "cs"), method = "gam", se = F) +
+      coord_cartesian(expand = FALSE) +
+      theme_bw()
+    ggsave(glue::glue("output/figures/{data_type}_{single_index_type}_smooth_gid_indicies.png"), plot2, width = 10, height = 10)
+  }
+  # ----------------------------------------------
+  return(NULL)
 }
